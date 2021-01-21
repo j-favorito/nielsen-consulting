a6dcfbf8350d2f0c44a8853b4f4514d9ae876d10
J Fav <jfavorito@outlook.com>
2021-01-20 19:49:16 -0700

    design changes

--- public/assets/js/navbar.js
+++ public/assets/js/navbar.js
@@ -1,7 +1,7 @@
 $(document).ready(function () { 
     let bootNav=$("<nav>")
     bootNav.attr({
-        class: "navbar navbar-expand-lg navbar-light bg-light"
+        class: "navbar navbar-expand-lg"
     });
     let brandElement=$("<a>");
     brandElement.attr({
